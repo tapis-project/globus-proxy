d7586888ad7ca28a4223d8bf45b973eb0e3e3b53
kprice <kprice@kprice01.tacc.utexas.edu>
2022-02-16 16:29:05 -0600

    eanup & comments

--- service/controllers.py
+++ service/controllers.py
@@ -29,32 +29,34 @@ class AuthURLResource(Resource):
     """
     def get(self, client_id):
         logger.debug("in get for tokens resource")
+        # init globus_sdk client
         client = globus_sdk.NativeAppAuthClient(client_id)
         client.oauth2_start_flow(refresh_tokens=True)
         client_uuid = uuid.uuid4()
+
+        # save client in memory
         local_client_uuid = locals()['client_uuid']
-        # current_app.locals()['client_id'] = client
         setattr(current_app, str(client_uuid), client)
-        logger.debug(f'in url ep, have current_app = {getattr(current_app, str(client_uuid))}')
 
         authorize_url = client.oauth2_get_authorize_url()
-        # print("Please go to this URL and login: {0}".format(authorize_url))
-        # return a uuid for use on the second endpoint
-        return utils.ok(result = {"url": authorize_url, "uuid": client_uuid}, msg = f'Please go to this URL and login: {authorize_url}')
+        return utils.ok(
+                result = {"url": authorize_url, "uuid": client_uuid}, 
+                msg = f'Please go to this URL and login: {authorize_url}')
 
 class TokensResource(Resource):
     """
     exchange auth code for access and refresh tokens
     """
     def get(self, uuid, auth_code):
-        # TODO: use the uuid from the first 
+        # retrieve client from memory
         client = getattr(current_app, uuid)
-        logger.debug(f'have client:: {client}')
-        
-        # client.oauth2_start_flow(refresh_tokens=True)
+
+        # get access / refresh tokens
         token_response = client.oauth2_exchange_code_for_tokens(auth_code)
-        logger.debug('got token response', token_response)
         access_token = token_response['access_token']
         refresh_token = token_response['refresh_token']
-        return utils.ok(result = {"access_token": access_token, "refresh_token": refresh_token}, msg = "successfully authorized globus client") 
+        
+        return utils.ok(
+                result = {"access_token": access_token, "refresh_token": refresh_token}, 
+                msg = "successfully authorized globus client") 
 
